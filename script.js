a701cefa0831641c59c9374a4f2e0160ca587e05
prathameshk9 <prathameshkulkarni2576@gmail.com>
2024-03-12 18:29:21 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,31 @@
+   
+
+   var isstatus = document.querySelector("h5")
+   var btn = document.querySelector("#add")
+   var check = 0
+
+
+  
+   btn.addEventListener("click",function(){
+    if(check == 0){
+       isstatus.innerHTML="Friends"
+       isstatus.style.color="green"
+       btn.innerHTML="Remove Friend"
+       check = 1
+    }else{
+        isstatus.innerHTML="Stranger"
+        isstatus.style.color="red"
+        btn.innerHTML="Add Friend"
+        check=0
+    }
+   })
+    
+
+
+
+// Another method -->>>
+//    var removeFriend = document.querySelector("#remove")
+//    removeFriend.addEventListener("click",function(){
+//     isstatus.innerHTML ="Stranger"
+//     isstatus.style.color="red"
+//    })
